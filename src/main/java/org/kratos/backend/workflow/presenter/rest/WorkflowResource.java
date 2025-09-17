a53d99dd98b7f38681babd81dcc7762482d371f9
Noman5237 <anonyman637@gmail.com>
2025-09-17 22:06:12 +0600

    wip: workflow dtos

--- src/main/java/org/kratos/backend/workflow/presenter/rest/WorkflowResource.java
+++ src/main/java/org/kratos/backend/workflow/presenter/rest/WorkflowResource.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WorkflowResource {
 	
-	// fixme(high): fix dtos here
+	// fixme(high): fix dto here
 	@PostMapping ("/create")
 	public Response<Void> run(@RequestHeader ("X-Subject") String wfConfigId
  
