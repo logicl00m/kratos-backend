e07f440c406e835b8eefd6257a48f846f0620773
Noman5237 <anonyman637@gmail.com>
2025-09-22 14:16:42 +0600

    transition in progress

--- src/main/java/org/kratos/backend/configuration/service/impl/SimpleStateTransitionHandler.java
+++ src/main/java/org/kratos/backend/configuration/service/impl/SimpleStateTransitionHandler.java
@@ -4,7 +4,9 @@ import org.jetbrains.annotations.NotNull;
 import org.kratos.backend.configuration.dto.TransitionResponse;
 import org.kratos.backend.configuration.service.StateTransitionHandler;
 import org.kratos.backend.workflow.service.impl.WorkflowContext;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SimpleStateTransitionHandler implements StateTransitionHandler {
 	
 	@Override
