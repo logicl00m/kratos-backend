f8d7a740a99f3e67b78a61aaec05a8ba53f5b8b3
Noman5237 <anonyman637@gmail.com>
2025-10-09 23:32:03 +0600

    updated schema with modified forms

--- src/main/java/org/kratos/backend/form/dto/FormCreateRequest.java
+++ src/main/java/org/kratos/backend/form/dto/FormCreateRequest.java
@@ -1,10 +1,10 @@
 package org.kratos.backend.form.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Map;
 
-public record FormCreateRequest(@com.fasterxml.jackson.annotation.JsonProperty("formJson")
-                                Map<String,Object> formJson) {}
-
-
+public record FormCreateRequest(@JsonProperty("formJson")
+                                Map<String,Object> formJson) {
 
 }
